ce8dd4399b2bb41665f985b249fdbc662d72acc6
caisepolente <steve.durighello@gmail.com>
2016-08-09 20:34:17 +0200

    select helper for subs and ports in model and view

--- app/models/port.rb
+++ app/models/port.rb
@@ -1,3 +1,11 @@
 class Port < ApplicationRecord
+
   has_many :submarines
+
+  def self.select_ports
+    all.order(name::asc).map do |port|
+      [port.name, port.id]
+    end
+  end
+
 end
--- app/models/submarine.rb
+++ app/models/submarine.rb
@@ -1,4 +1,12 @@
 class Submarine < ApplicationRecord
+
   belongs_to :port
   has_many :bookings
+
+  def self.select_submarines
+    all.order(name::asc).map do |submarine|
+      [submarine.name, submarine.id]
+    end
+  end
+
 end
