ba4a7a5331cb514c5cb75f83c0594f827abac302
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-08-10 18:24:21 +0200

    add check_available_submarines method

--- app/models/port.rb
+++ app/models/port.rb
@@ -1,6 +1,8 @@
 class Port < ApplicationRecord
 
+
   has_many :submarines
+
 
   def self.select_ports
     all.order(name::asc).map do |port|
@@ -8,7 +10,34 @@ class Port < ApplicationRecord
     end
   end
 
-  def check_availability
-  end
+  # Finds available submarines for a port within a given period
+  # port should be a Port object
+  # desired_start_at, desired_end_at should be Date objects
+  def check_available_submarines(port, desired_start_at, _desired_end_at)
+    # Find all submarines for a port
+    submarines = port.submarines
+    available_submarines = []
 
+    # All bookings for a submarine
+    submarines.each do |submarine|
+      bookings = submarine.bookings
+
+      # Check desired booking dates against existing bookings
+      bookings.each do |booking|
+        if booking.start_at < desired_start_at && booking.end_at > desired_start_at
+          sub_free = false
+        elsif booking.start_at < desired_end_at && booking.end_at > desired_end_at
+          sub_free = false
+        elsif booking.start_at > desired_start_at && booking.end_at < desired_end_at
+          sub_free = false
+        end
+      end
+
+      # Check sub_free flag
+      if sub_free
+        available_submarines << submarine
+      end
+    end
+
+  end
 end
