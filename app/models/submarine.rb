42042a5e93338c15e29605e5dd2dd1312a4322aa
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-08-12 12:43:45 +0200

    working on availability

--- app/models/submarine.rb
+++ app/models/submarine.rb
@@ -9,4 +9,28 @@ class Submarine < ApplicationRecord
     end
   end
 
+  # Finds available submarines for a port within a given period
+  # submarine should be a Submarine object
+  # desired_start_at, desired_end_at should be Date objects
+  def validate_availability(submarine, desired_start_at, desired_end_at)
+
+    Booking.where("start_at BETWEEN ? AND ?, OR end_at BETWEEN ? AND ?",
+                desired_start_at, desired_end_at, desired_start_at, desired_end_at)
+  end
+
+  def validate_future_days(desired_start_at, desired_end_at)
+    if desired_start_at > Date.today
+      errors.add "Start date must be in the future."
+    end
+
+    if desired_end_at > Date.today
+      errors.add "End date must be in the future."
+    end
+  end
+
+  def validate_start_date_before_end_date
+    if desired_end_at > desired_start_at
+      errors.add "Your end date may not precede your start date."
+    end
+  end
 end
